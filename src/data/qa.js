6d6ab2672c760c565a163cef47eb779aaa9b82f8
kaneko_n <kaneko_n@thinkone.jp>
2023-08-25 16:43:39 +0900

    TODO完成

--- src/data/qa.js
+++ src/data/qa.js
@@ -2,7 +2,7 @@ export const qaList = [
   { name: "1-1", url: "https://forms.gle/eWVpSLzP8hebY1oF9" },
   { name: "1-2", url: "https://forms.gle/7c5HK7HAHJHRhUdq5" },
   { name: "1-3", url: "https://forms.gle/tvLcXKQsrgwNcfpy5" },
-  { name: "1-4", url: "" },
-  { name: "1-5", url: "" },
-  { name: "1-6", url: "" },
+  { name: "1-4", url: "https://forms.gle/ryRd7KKgyYAee95D9" },
+  { name: "1-5", url: "/" },
+  { name: "1-6", url: "/" },
 ];
