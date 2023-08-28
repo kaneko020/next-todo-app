ebab290eefdafccfae1e0efc028a0ee3ca30002f
kaneko_n <kaneko_n@thinkone.jp>
2023-08-28 16:47:23 +0900

    Q＆A追加

--- src/data/qa.js
+++ src/data/qa.js
@@ -1,8 +1,16 @@
-export const qaList = [
+export const qaList1 = [
   { name: "1-1", url: "https://forms.gle/eWVpSLzP8hebY1oF9" },
   { name: "1-2", url: "https://forms.gle/7c5HK7HAHJHRhUdq5" },
   { name: "1-3", url: "https://forms.gle/tvLcXKQsrgwNcfpy5" },
   { name: "1-4", url: "https://forms.gle/ryRd7KKgyYAee95D9" },
   { name: "1-5", url: "https://forms.gle/HDRkB9Mu8dgAqGK39" },
   { name: "1-6", url: "https://forms.gle/N1y431qZoR5DYkZi8" },
+];
+
+export const qaList2 = [
+  { name: "2-1", url: "https://forms.gle/pMRTCpCFYSRo8WfG7" },
+  { name: "2-2", url: "https://forms.gle/nPXrjcsR3W4JszmR9" },
+  { name: "2-3", url: "https://forms.gle/2AMUDJ6mdLpmKS449" },
+  { name: "2-5", url: "https://forms.gle/kPYScm9hiNCvWn7D8" },
+  { name: "2-6", url: "https://forms.gle/GWEgSCv8AekkYC5x7" },
 ];
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Title from "../components/title";
-import { qaList } from "../data/qa";
+import { qaList1 } from "../data/qa";
+import { qaList2 } from "../data/qa";
 
 export default function Home() {
   return (
@@ -15,18 +16,34 @@ export default function Home() {
 
         <div className="my-8">
           <h2 className="my-4 text-xl font-semibold">Q&A</h2>
-          {qaList.map((item) => {
-            return (
-              <Link href={item.url} key={item.name}>
-                <a
-                  className="mx-2 p-2 bg-gray-100 text-gray-600 text-[17px] font-semibold rounded-[10px] drop-shadow hover:text-white hover:bg-gray-400"
-                  target="_blank"
-                >
-                  {item.name}
-                </a>
-              </Link>
-            );
-          })}
+          <div className="my-6">
+            {qaList1.map((item) => {
+              return (
+                <Link href={item.url} key={item.name}>
+                  <a
+                    className="mx-2 p-2 bg-gray-100 text-gray-600 text-[17px] font-semibold rounded-[10px] drop-shadow hover:text-white hover:bg-gray-400"
+                    target="_blank"
+                  >
+                    {item.name}
+                  </a>
+                </Link>
+              );
+            })}
+          </div>
+          <div className="my-10">
+            {qaList2.map((item) => {
+              return (
+                <Link href={item.url} key={item.name}>
+                  <a
+                    className="mx-2 p-2 bg-gray-100 text-gray-600 text-[17px] font-semibold rounded-[10px] drop-shadow hover:text-white hover:bg-gray-400"
+                    target="_blank"
+                  >
+                    {item.name}
+                  </a>
+                </Link>
+              );
+            })}
+          </div>
         </div>
       </div>
     </>
